1856d573b2e98347b78aa9adb04eca1ab972d6e9
Pam Harris <pharris@nrao.edu>
2022-10-31 15:31:40 -0600

    Implement additional axes

--- casagui/apps/_plotms.py
+++ casagui/apps/_plotms.py
@@ -15,6 +15,8 @@ from bokeh.models.formatters import BasicTickFormatter, DatetimeTickFormatter
 from bokeh.plotting import figure
 
 from cngi_io import read_ms
+from casacore.quanta import constants, quantity
+from casacore.measures import measures
 
 try:
     from cairosvg import svg2pdf
@@ -22,11 +24,27 @@ try:
 except ImportError:
     _have_svg2pdf = False
 
-_MIN_PLOT_WIDTH = 600
-_MIN_PLOT_HEIGHT = 500
+# hvplot defaults
+_PLOT_WIDTH = 700
+_PLOT_HEIGHT = 300
+
+def _get_freq_params(xds, xaxis, yaxis):
+    # Return frequency parameters (frame, unit, rest frequency) from spw table
+    freq_axes = ['frequency', 'velocity', 'uwave', 'vwave', 'wwave', 'uvwave']
+    if xaxis in freq_axes or yaxis in freq_axes:
+        spw_table = xds.SPECTRAL_WINDOW
+        ref_types = spw_table.column_descriptions['CHAN_FREQ']['keywords']['MEASINFO']['TabRefTypes']
+        freq_frame = ref_types[spw_table.MEAS_FREQ_REF.values[0]]
+        freq_unit = spw_table.column_descriptions['CHAN_FREQ']['keywords']['QuantumUnits'][0]
+        freqs = spw_table.CHAN_FREQ.values[0]
+        rest_freq = freqs[int(len(freqs) / 2)]
+        return freq_frame, freq_unit, rest_freq
+
+    return "", "", None
 
 def _setup_axis(axis):
-    labels = {'uvdist': 'UVdist', 'wtxamp': 'Wt*Amp', 'wtsp': 'WtSp', 'sigmasp': 'SigmaSp'}
+    labels = {'uvdist': 'UVdist', 'uvwave': 'UVwave', 'wtxamp': 'Wt*Amp',
+        'wtsp': 'WtSp', 'sigmasp': 'SigmaSp'}
     if axis in labels:
         label = labels[axis]
     else:
@@ -35,23 +53,32 @@ def _setup_axis(axis):
     if axis == "time":
         date_format = ['%Y-%m-%d %H:%M:%S']
         time_format = ['%H:%M:%S']
-        formatter = DatetimeTickFormatter(days=date_format, months=date_format, years=date_format, hours=time_format, minutes=time_format, seconds=time_format)
+        formatter = DatetimeTickFormatter(days=date_format, months=date_format,
+            years=date_format, hours=time_format, minutes=time_format, seconds=time_format)
     else:
         formatter = BasicTickFormatter()
 
     return (label, formatter)
 
-def _add_axis_unit(axis, first_time):
-    units = {"Interval": " (s)", "Frequency": " (GHz)", "Velocity": " (km/s)", "U": " (m)",
-             "V": " (m)", "W": " (m)", "Uvdist": " (m)", "Phase": " (deg)"}
-    if axis in units:
-        return axis + units[axis]
-    elif axis == "Time":
+def _add_axis_unit(axis_name, first_time, freq_frame):
+    units = {"Interval": " (s)", "Frequency": " (GHz)", "Velocity": " (km/s)",
+        "Phase": " (deg)"}
+    if axis_name in units:
+        label = axis_name + units[axis_name]
+        if axis_name == "Frequency":
+            label = label + " " + freq_frame
+    elif axis_name == "Time":
         time_string = np.datetime_as_string(first_time)
         date = time_string[0 : time_string.find("T")]
-        return axis + " (from " + date + ")"
+        label = axis_name + " (from " + date + ")"
+    elif axis_name in ["U", "V", "W", "UVdist"]:
+        label = axis_name + " (m)"
+    elif axis_name in ["Uwave", "Vwave", "Wwave", "UVwave"]:
+        label = axis_name + " (\u03BB)"
     else:
-        return axis
+        label = axis_name
+
+    return label
 
 def _apply_flags(xds):
     flg_xds = xds.copy()
@@ -66,15 +93,13 @@ def _apply_flags(xds):
 
 def _get_plot_dataset(xds, xkey, ykey, xaxis, yaxis):
     # Dataset containing plotted axes only
-    # Coordinates
+    # Coordinates: remove uvw_index since already indexed to get u, v, w
+    xdims = tuple(dim for dim in _get_array_dims(xds, xkey) if dim != 'uvw_index')
+    ydims = tuple(dim for dim in _get_array_dims(xds, ykey) if dim != 'uvw_index')
+    plot_dims = set(xdims + ydims)
     plot_coords = {}
-    xdims = list(_get_array_dims(xds, xkey))
-    ydims = list(_get_array_dims(xds, ykey))
-    # Remove uvw_index since already indexed to get u, v, w
-    if 'uvw_index' in xdims: xdims.remove('uvw_index')
-    if 'uvw_index' in ydims: ydims.remove('uvw_index')
-    for dim in xdims: plot_coords[dim]=xds.coords[dim]
-    for dim in ydims: plot_coords[dim]=xds.coords[dim]
+    for dim in plot_dims:
+        plot_coords[dim] = xds.coords[dim]
 
     # Data variables
     xvals = _get_data(xds, xkey, xaxis)
@@ -92,8 +117,19 @@ def _get_plot_dataset(xds, xkey, ykey, xaxis, yaxis):
     return plot_xds
 
 def _get_array_dims(xds, key):
-    if isinstance(key, tuple): # axis calculated from two values
-        return tuple(set(_get_array_dims(xds, key[0]) + _get_array_dims(xds, key[1])))
+    if isinstance(key, tuple):
+        # Axis calculated from two values
+        dims0 = _get_array_dims(xds, key[0])
+        dims1 = _get_array_dims(xds, key[1])
+        if dims0 == dims1:
+            return dims0
+        elif set(dims0).issubset(dims1):
+            return dims1
+        elif set(dims1).issubset(dims0):
+            return dims0
+        else:
+            # uwave, vwave, wwave (UVW, chan) put chan first
+            return dims1 + dims0
     if key in xds.data_vars:
         return xds.data_vars[key].dims
     elif key in xds.coords:
@@ -113,18 +149,18 @@ def _get_array(xds, key):
     return xda
 
 def _get_values(xda, key, axis):
-    if key == 'UVW':
-        if axis == "u":
+    if key == "UVW":
+        if axis in ["u", "uwave"]:
             return xda.sel(uvw_index=0).values
-        if axis == "v":
+        if axis in ["v", "vwave"]:
             return xda.sel(uvw_index=1).values
-        if axis == "w":
+        if axis in ["w", "wwave"]:
             return xda.sel(uvw_index=2).values
         if axis == "uvdist":
             u = xda.sel(uvw_index=0).values
             v = xda.sel(uvw_index=1).values
             return np.sqrt(u*u + v*v)
-    elif key == 'DATA':
+    elif key == "DATA":
         if axis in ["amp", "wtxamp"]:
             return np.abs(xda.values)
         if axis == "phase":
@@ -133,29 +169,37 @@ def _get_values(xda, key, axis):
             return np.real(xda.values)
         if axis == "imag":
             return np.imag(xda.values)
-    elif key == 'chan':
-        if axis == 'channel':
+    elif key == "chan":
+        if axis == "channel":
             return np.array(range(xda.values.size))
-        if axis == 'frequency':
-            values = xda.values
-            if isinstance(values[0], tuple): # chan is common dimension
-                values = np.array([val[0] for val in values])
-            return values / 1.0e9 #GHz
-    elif key == 'FLAG':
+    elif key == "FLAG":
         return xda.values.astype(int)
 
     return xda.values
 
 def _get_calc_values(xds, key, axis):
     # axis calculated from two values
+    if (len(key) < 2):
+        raise RuntimeError(f"Cannot calculate {axis} from one column")
+
     data0 = _get_data(xds, key[0], axis)
     data1 = _get_data(xds, key[1], axis)
 
-    if axis == 'wtxamp':
-        return data0 * data1
-    elif axis == 'baseline':
+    if axis == "wtxamp":
+        # data0=WEIGHT, data1=AMP.  Multiply each channel plane by weight matrix
+        for i in range(xds.dims['chan']):
+            data1[:,i,:] *= data0
+        return data1
+    elif axis == "baseline":
+        # data0=ANTENNA1, data1=ANTENNA2
         num_ant = max(max(data0), max(data1)) + 1
         return np.array([(num_ant + 1) * ant1 - (ant1 * ant1 + 1) / 2 + ant2 for ant1, ant2 in zip(data0, data1)])
+    elif "wave" in axis:
+        # data0=U/V/W (row), data1=FREQ (chan)
+        wave = np.zeros(shape=(xds.dims['chan'], xds.dims['row']))
+        for i in range(xds.dims['row']):
+            wave[:, i] = (data0[i] / constants['c'].get_value()) * (data1)
+        return wave
     else:
         raise RuntimeError(f"axis {axis} not supported")
 
@@ -165,6 +209,32 @@ def _get_data(xds, key, axis):
     else:
         xda = _get_array(xds, key)
         return _get_values(xda, key, axis)
+
+def _calc_freq(xds, freq_unit):
+    if freq_unit != "GHz":
+        freqs = xds.frequency.values
+        dm = measures()
+        freqs_GHz = []
+
+        for freq in freqs:
+            freq_quant = quantity(freq, freq_unit)
+            freqs_GHz.append(freq_quant.get_value('GHz'))
+
+        xds.frequency.values = np.array(freqs_GHz)
+
+def _calc_velocity(xds, freq_frame, freq_unit, rest_freq):
+    freqs = xds.velocity.values
+    dm = measures()
+    velocities = []
+    rf = dm.frequency(freq_frame, quantity(rest_freq, freq_unit))
+
+    for freq in freqs:
+        f = dm.frequency(freq_frame, quantity(freq, freq_unit))
+        v = dm.todoppler('radio', f, rf)['m0']
+        vq = quantity(v['value'], v['unit'])
+        velocities.append(vq.get_value('km/s'))
+
+    xds.velocity.values = np.array(velocities)
 
 def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True):
     """Plot visibility axes and show interactive plot or export to file.
@@ -183,6 +253,20 @@ def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True)
         Filename for exported plot
     showplot (bool):
         Whether to show interactive plot in browser tab
+
+    Axis options:
+        NOTE: Items in parentheses not implemented yet
+        METADATA
+            scan, field, time, interval, spw, channel, frequency,
+            velocity, corr, antenna1, antenna2, baseline, corr, antenna1,
+            antenna2, baseline, observation, intent, feed1, feed2
+        VISIBILITIES and FLAGS
+            amp, phase, real, imag, weight, wtxamp, wtsp, sigma, sigmasp, flag
+        OBSERVATIONAL GEOMETRY
+            u, v, w, uvdist, uwave, vwave, wwave, (uvwave),
+            (azimuth), (elevation), (hourang), (parang)
+        ANTENNA-BASED GEOMETRY
+            (antenna), (ant-az), (ant-el), (ant-ra), (ant-dec), (ant-parang)
     """
 
     # Check arguments
@@ -197,7 +281,7 @@ def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True)
                  'spw': 'spw_id',
                  'channel': 'chan',
                  'frequency': 'chan',
-                 #'velocity': 'freq',
+                 'velocity': 'chan',
                  'corr': 'pol',
                  'antenna1': 'ANTENNA1',
                  'antenna2': 'ANTENNA2',
@@ -212,7 +296,7 @@ def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True)
                  'real': 'DATA',
                  'imag': 'DATA',
                  'weight': 'WEIGHT',
-                 ##'wtxamp': ('WEIGHT', 'DATA'),
+                 'wtxamp': ('WEIGHT', 'DATA'),
                  'wtsp': 'WEIGHT_SPECTRUM',
                  'sigma': 'SIGMA', 
                  'sigmasp': 'SIGMA_SPECTRUM', 
@@ -221,11 +305,11 @@ def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True)
                  'u': 'UVW',
                  'v': 'UVW',
                  'w': 'UVW',
-                 'uvdist': 'UVW'
-                 #'uwave': 'UVW',
-                 #'vwave': 'UVW',
-                 #'wwave': 'UVW',
-                 #'uvwave': 'UVW',
+                 'uvdist': 'UVW',
+                 'uwave': ('UVW', 'chan'),
+                 'vwave': ('UVW', 'chan'),
+                 'wwave': ('UVW', 'chan'),
+                 #'uvwave': ('UVW', 'chan'),
                  #'azimuth': '',
                  #'elevation': '',
                  #'hourang': '',
@@ -249,8 +333,17 @@ def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True)
 
     # Read ms into xarray dataset
     print("Reading MeasurementSet")
-    ms_xds = read_ms(vis)
-    first_time = ms_xds.xds0.data_vars["TIME"].values[0]
+    ms_xds = read_ms(infile=vis)
+
+    # Extra info for plotting certain axes
+    # Square plot for UV
+    uvw = ['u', 'v', 'w']
+    is_uv_plot = xaxis in uvw and yaxis in uvw
+    plot_width = _PLOT_WIDTH
+    plot_height = _PLOT_WIDTH if is_uv_plot else _PLOT_HEIGHT
+
+    first_time = ms_xds.xds0.TIME.values[0]
+    freq_frame, freq_unit, rest_freq = _get_freq_params(ms_xds, xaxis, yaxis)
 
     # Set up axis labels and plot title (with simple axis labels)
     xlabel, xformatter = _setup_axis(xaxis)
@@ -258,8 +351,9 @@ def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True)
     if not title:
         msname = os.path.basename(vis)
         title = msname + " " + ylabel + " vs. " +  xlabel
-    xlabel = _add_axis_unit(xlabel, first_time)
-    ylabel = _add_axis_unit(ylabel, first_time)
+
+    xlabel = _add_axis_unit(xlabel, first_time, freq_frame)
+    ylabel = _add_axis_unit(ylabel, first_time, freq_frame)
 
     nspw = ms_xds.dims['spw_ids']
     spw_ids = ms_xds.coords['spw_ids'].values
@@ -274,17 +368,32 @@ def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True)
         flg_xds = _apply_flags(xds)
         plot_xds = _get_plot_dataset(flg_xds, xkey, ykey, xaxis, yaxis)
 
+        if "frequency" in plot_xds.data_vars:
+            # Convert to GHz
+            _calc_freq(plot_xds, freq_unit)
+        if "velocity" in plot_xds.data_vars:
+            # Convert freq to velocity km/s
+            _calc_velocity(plot_xds, freq_frame, freq_unit, rest_freq)
+
         # Convert to dataframe and plot
         plot_df = plot_xds.to_dataframe()
         plot = plot_df.hvplot.scatter(xaxis, yaxis,
             title=title, xlabel=xlabel, ylabel=ylabel,
             xformatter=xformatter, yformatter=yformatter,
-            responsive=True, min_width=_MIN_PLOT_WIDTH, min_height=_MIN_PLOT_HEIGHT,
+            width=plot_width, height=plot_height,
             rasterize=True, cmap=['blue'], padding=(0.01, 0.01), hover=True)
 
-        if spw == 0:
-            layout = plot
-        else:
+        if spw == 0: layout = plot
+        else:        layout = layout * plot
+
+        if is_uv_plot:
+            # plot conjugates
+            conj_plot_df = plot_df * -1.0
+            plot = conj_plot_df.hvplot.scatter(xaxis, yaxis,
+                title=title, xlabel=xlabel, ylabel=ylabel,
+                xformatter=xformatter, yformatter=yformatter,
+                width=plot_width, height=plot_height,
+                rasterize=True, cmap=['blue'], padding=(0.01, 0.01), hover=True)
             layout = layout * plot
 
     if showplot:
@@ -298,7 +407,7 @@ def plotms(vis, xaxis='time', yaxis='amp', title="", plotfile="", showplot=True)
             hvplot.save(layout, plotfile)
         elif plotfile.endswith(".svg"):
             bokeh_plot = hv.render(layout)
-            # show(bokeh_plot) has points, svg plot does not
+            # show(bokeh_plot) has points in plot, svg plot does not
             bokeh_plot.output_backend = "svg"
             export_svg(bokeh_plot, filename=plotfile)
         elif plotfile.endswith(".pdf"):
