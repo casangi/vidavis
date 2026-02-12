6f954859ead98ffc1d09e1eee8e93c2355a84f83
Pam Harris <pharris@nrao.edu>
2026-02-12 09:12:59 -0700

    Update version number in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2025 Associated Universities, Inc. Washington DC, USA.'
 author = 'CASA visualization team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.7'
+release = '0.1.8'
 
 
 # -- General configuration ---------------------------------------------------
